18090f3cd86c37f0141dc2b71639aa89ace9a1cc
Fig77 <55416179+Fig77@users.noreply.github.com>
2019-11-19 14:31:03 -0300

    finished the header fixed settings. Need to add width validation!

--- js/main.js
+++ js/main.js
@@ -18,24 +18,27 @@ window.onscroll = function () {
 
 function changeNav() {
   
-  var redNav = document.getElementById("red-nav-bar");
+  var redNav =  document.getElementById("red-nav-bar");
   var dateNav = document.getElementById("date-red-nav");
   var logoNav = document.getElementById("logo-red-nav");
+  var hamb = document.getElementById("hamburguer-b");
+  var cont = document.getElementById("cont-but-hamb");
 
-  if (((verticalY >= 102) && verticalY <= 190) && boolOff) {
+  if (((verticalY >= 102) && verticalY <= 250) && boolOff) {
     dateNav.classList.remove("d-lg-block");
     logoNav.classList.add("logo-nav-bar-f");
     redNav.classList.add("red-nav-bar-f");
     redNav.classList.remove("pb-lg-4","pb-lg-4","pb-sm-1","pr-3","pl-3","pb-1");
+    hamb.classList.remove("d-xl-none","d-lg-none");
+    redNav.classList.add("pl-1","pr-1");
   }
   
   if ((verticalY <= 105) && !boolOff ) {
-    document.getElementById("red-nav-bar").classList.remove("red-nav-bar-f");
+    redNav.classList.remove("red-nav-bar-f");
     dateNav.classList.add("d-lg-block");
     logoNav.classList.remove("logo-nav-bar-f");
     redNav.classList.add("pb-lg-4","pb-lg-4","pb-sm-1","pr-3","pl-3","pb-1");
-    
-    
+    hamb.classList.add("d-xl-none","d-lg-none");
   }
 
 };
