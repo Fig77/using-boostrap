7f03a039782f0f54db114ae92c15f2a37624bcaa
Fig77 <55416179+Fig77@users.noreply.github.com>
2019-11-19 16:11:25 -0300

    added validation to header JS, fixed some resizing

--- js/main.js
+++ js/main.js
@@ -1,44 +1,64 @@
+//TODO: Width validation, F5 validation.
+
 var verticalY;
 var offSet = 0;
-var boolOff=true;
+var boolOff = true;
+var formChanged = false;
 
 window.onscroll = function () {
   verticalY = window.scrollY;
- 
-  if (offSet < verticalY) {
-    boolOff = true;
+  if (window.innerWidth >= 992) {
+    if (offSet < verticalY) {
+      boolOff = true;
+    } else {
+      boolOff = false;
+    }
+    offSet = verticalY;
+    //console.log(verticalY);
+
+    changeNav();
   }
-  else {
-    boolOff = false;
-  }
-  offSet = verticalY;
-  //console.log(verticalY);
-  changeNav();
 };
 
+// Header
 function changeNav() {
-  
-  var redNav =  document.getElementById("red-nav-bar");
+
+  var redNav = document.getElementById("red-nav-bar");
   var dateNav = document.getElementById("date-red-nav");
   var logoNav = document.getElementById("logo-red-nav");
   var hamb = document.getElementById("hamburguer-b");
-  var cont = document.getElementById("cont-but-hamb");
+
 
   if (((verticalY >= 102) && verticalY <= 250) && boolOff) {
     dateNav.classList.remove("d-lg-block");
     logoNav.classList.add("logo-nav-bar-f");
     redNav.classList.add("red-nav-bar-f");
-    redNav.classList.remove("pb-lg-4","pb-lg-4","pb-sm-1","pr-3","pl-3","pb-1");
-    hamb.classList.remove("d-xl-none","d-lg-none");
-    redNav.classList.add("pl-1","pr-1");
+    redNav.classList.remove("pb-lg-4", "pb-lg-4", "pb-sm-1", "pr-3", "pl-3", "pb-1");
+    hamb.classList.remove("d-xl-none", "d-lg-none");
+    redNav.classList.add("pl-1", "pr-1");
   }
-  
-  if ((verticalY <= 105) && !boolOff ) {
+
+  if ((verticalY <= 105) && !boolOff) {
     redNav.classList.remove("red-nav-bar-f");
     dateNav.classList.add("d-lg-block");
     logoNav.classList.remove("logo-nav-bar-f");
-    redNav.classList.add("pb-lg-4","pb-lg-4","pb-sm-1","pr-3","pl-3","pb-1");
-    hamb.classList.add("d-xl-none","d-lg-none");
+    redNav.classList.add("pb-lg-4", "pb-lg-4", "pb-sm-1", "pr-3", "pl-3", "pb-1");
+    redNav.classList.remove("pl-1", "pr-1");
+    hamb.classList.add("d-xl-none", "d-lg-none");
   }
 
 };
+
+//Searchbar
+
+function enlargeSearchbar() {
+  var search = document.getElementById("search-bar");
+  if (!formChanged) {
+    search.classList.add("input-bar-on-click")
+    formChanged = true;
+  } else {
+    search.classList.remove("input-bar-on-click")
+    formChanged = false;
+  }
+
+}
